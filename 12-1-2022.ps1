bd84158f4a71e33afd7ce160ef0b2419390b051a
Ben Siebers <66695930+WatchBenGo@users.noreply.github.com>
2022-12-02 09:33:04 -0600

    Files from 12-1

---
+++ 12-1-2022.ps1
@@ -0,0 +1,42 @@
+﻿#First Part
+$Numbers = Get-Content 'C:\Users\bsiebers\Documents\Personal\Advent of Code\Numbers.txt'
+$Max = 0
+
+
+ForEach ($Row in $Numbers) {
+    $Split = $Row.Split(',')
+    $Total = 0
+    ForEach ($Entry in $Split) {
+        $Total = $Total + $Entry
+    }
+    If ($Total -gt $Max) {
+        $Max = $Total
+    }
+}
+$Max
+
+
+#Second Part
+$Numbers = Get-Content 'C:\Users\bsiebers\Documents\Personal\Advent of Code\Numbers.txt'
+$Max = 0
+$Totals = @()
+
+
+ForEach ($Row in $Numbers) {
+    $Split = $Row.Split(',')
+    $Total = 0
+    ForEach ($Entry in $Split) {
+        $Total = $Total + $Entry
+    }
+    If ($Total -gt $Max) {
+        $Max = $Total
+    }
+    $Totals += $Total
+}
+$TopThree = $Totals | Sort -Descending | Select -First 3
+
+$SumTopThree = 0
+ForEach ($Elf in $TopThree) {
+    $SumTopThree = $SumTopThree + $Elf
+}
+$SumTopThree
