d482889979cb99fd9035f4c95b8beee98f100706
Ben Siebers <66695930+WatchBenGo@users.noreply.github.com>
2022-12-06 23:50:44 -0600

    Add files via upload

---
+++ 12-7-2022.ps1
@@ -0,0 +1,45 @@
+﻿$Commands = Get-Content C:\Users\Ben\Desktop\TestFileSystem.txt
+
+#Commands
+#cd <foldername>
+#cd .. (Up one level)
+#cd / (top level)
+#ls - List
+    #123 ABC - File named ABC w/ size 123
+    #dir xyz - directory named xyz
+
+$FileSystem = [PSCustomObject]@{
+    Directories = [System.Collections.ArrayList]@()
+    Files = [System.Collections.ArrayList]@()
+}
+
+$CurrentDirectory = ""
+
+ForEach ($Line in $Commands) {
+    #Current Directory
+    If ($Line[0] -eq "$") {
+        #Command
+        $Split = $Line.Split(' ')
+        Switch ($Split[1]) {
+            cd {
+                Switch ($Split[2]) {
+                    / {
+                        $CurrentDirectory = $FileSystem
+                    }
+                    .. {
+                        $CurrentDirectory = $CurrentDirectory.ParentDirectory
+                    }
+                    Default {
+                        
+                    }
+                }
+                
+            }
+            ls {
+                #Do I need to do anything?
+            }
+        }
+    } Else {
+        #Data
+    }
+}
