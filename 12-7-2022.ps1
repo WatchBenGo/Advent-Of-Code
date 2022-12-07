ca30f0bc53c9cba764c4edd397f25c4a5dec71f3
WatchBenGo <ben@thesiebers.net>
2022-12-07 14:01:33 -0600

    Working Code for 12-7

--- 12-7-2022.ps1
+++ 12-7-2022.ps1
@@ -1,45 +1,64 @@
-﻿$Commands = Get-Content C:\Users\Ben\Desktop\TestFileSystem.txt
+﻿$Commands = Get-Content "C:\Users\bsiebers\Documents\Personal\Advent of Code\FileSystem.txt"
+$FileSizes = @()
 
-#Commands
-#cd <foldername>
-#cd .. (Up one level)
-#cd / (top level)
-#ls - List
-    #123 ABC - File named ABC w/ size 123
-    #dir xyz - directory named xyz
+$CurrentPath = ""
+$Directories = @{}
 
-$FileSystem = [PSCustomObject]@{
-    Directories = [System.Collections.ArrayList]@()
-    Files = [System.Collections.ArrayList]@()
-}
-
-$CurrentDirectory = ""
-
-ForEach ($Line in $Commands) {
-    #Current Directory
-    If ($Line[0] -eq "$") {
-        #Command
-        $Split = $Line.Split(' ')
-        Switch ($Split[1]) {
-            cd {
-                Switch ($Split[2]) {
-                    / {
-                        $CurrentDirectory = $FileSystem
-                    }
-                    .. {
-                        $CurrentDirectory = $CurrentDirectory.ParentDirectory
-                    }
-                    Default {
-                        
-                    }
-                }
-                
+ForEach($Line in $Commands) {
+    If($Line -match "^\$\s") {
+        #Commands
+        Switch -Regex ($Line) {
+            '^\$\sls' {
+                #No need to do anything
+                Continue
             }
-            ls {
-                #Do I need to do anything?
+            '^\$\scd\s(?<Dir>\w+)' {
+                $CurrentPath += "/$($Matches.Dir)"
+                $Directories.Add($CurrentPath,0)
+            }
+            '^\$\scd\s\.\.' {
+                $CurrentPath = $CurrentPath.Substring(0,$CurrentPath.LastIndexOf('/'))
             }
         }
-    } Else {
+    }
+    Else { 
         #Data
+        If($Line -match "^dir"){
+            #No need to do anything
+            Continue
+        }
+        Else {
+            $Line -match "^(?<Size>\d+)\s(?<Filename>.*)$" | Out-Null
+            $FileSizes += $Matches.Size
+
+            $TempPath = $CurrentPath + "/"
+            $ParentDirs = @()
+            while($TempPath.Length -gt 1){
+                $TempPath = $TempPath.Substring(0,$TempPath.LastIndexOf('/'))
+                $ParentDirs += $TempPath
+            }
+            ForEach($Dir in $ParentDirs){
+                If($Dir.Length -gt 0) {
+                    $Directories.$Dir += $Matches.Size
+                }
+            }
+        }
     }
 }
+
+#Part 1
+$Sum = 0
+$Directories.GetEnumerator() | where {$_.Value -le 100000} | % {$Sum += $_.Value}  
+Write-Host $Sum
+
+#Part 2
+$RootSum = 0
+ForEach($Row in $FileSizes) {
+    $RootSum += [int]$Row
+}
+
+$Free = 70000000 - $RootSum
+$Needed = 30000000 - $Free
+$Remove = $Directories.GetEnumerator() | where {$_.Value -ge $Needed} | Sort-Object -Property Value | Select-Object -First 1
+
+Write-Host $Remove.Value
