6d4d22827bda0d83199b9c6b9cac20fc73f06ab9
WatchBenGo <ben@thesiebers.net>
2022-12-12 16:33:38 -0600

    Making Folders

---
+++ 2022/12-2/12-2-2022.ps1
@@ -0,0 +1,78 @@
+﻿#Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock
+
+#A for Rock, B for Paper, and C for Scissors
+#X for Rock, Y for Paper, and Z for Scissors
+
+
+
+#The score for a single round is the score for the shape you selected 
+#(1 for Rock, 2 for Paper, and 3 for Scissors) 
+#plus the score for the outcome of the round 
+#(0 if you lost, 3 if the round was a draw, and 6 if you won)
+
+
+$Data = Import-Csv C:\Users\Ben\Desktop\Input.csv
+$TotalScore = 0
+
+ForEach ($Round in $Data) {
+    $Opponent = ""
+    $Player = ""
+    $Score = 0
+
+    Switch ($Round.Opponent) {
+        A {$Opponent = "Rock"}
+        B {$Opponent = "Paper"}
+        C {$Opponent = "Scissors"}
+        Default {Throw "Error"}
+    }
+    Switch ($Round.Player) {
+        X {
+            $Player = "Rock"
+            $Score += 1
+        }
+        Y {
+            $Player = "Paper"
+            $Score += 2
+        }
+        Z {
+            $Player = "Scissors"
+            $Score += 3
+        }
+        Default {Throw "Error"}
+    }
+    #Write-Host "$Opponent vs. $Player"
+
+    If ($Opponent -eq "Rock" -and $Player -eq "Paper"){
+        $Result = "Yes"
+    } ElseIf ($Opponent -eq "Rock" -and $Player -eq "Scissors"){
+        $Result = "No"
+    } ElseIf ($Opponent -eq "Rock" -and $Player -eq "Rock"){
+        $Result = "Tie"
+    }
+
+    If ($Opponent -eq "Paper" -and $Player -eq "Scissors"){
+        $Result = "Yes"
+    } ElseIf ($Opponent -eq "Paper" -and $Player -eq "Rock"){
+        $Result = "No"
+    } ElseIf ($Opponent -eq "Paper" -and $Player -eq "Paper"){
+        $Result = "Tie"
+    }
+
+    If ($Opponent -eq "Scissors" -and $Player -eq "Rock"){
+        $Result = "Yes"
+    } ElseIf ($Opponent -eq "Scissors" -and $Player -eq "Paper"){
+        $Result = "No"
+    } ElseIf ($Opponent -eq "Scissors" -and $Player -eq "Scissors"){
+        $Result = "Tie"
+    }
+    
+    Switch ($Result) {
+        Yes {$Score += 6}
+        No {}
+        Tie {$Score += 3}
+        Default { Throw "Result Error"}
+    }
+    $TotalScore += $Score
+}
+
+$TotalScore
---
+++ 2022/12-3/12-3-2022.ps1
@@ -0,0 +1,52 @@
+﻿$Data = Get-Content C:\Users\Ben\Desktop\Data.txt
+
+$Total = 0
+
+ForEach ($Backpack in $Data) {
+    $Length = $Backpack.Length
+    $Middle = $Length /2
+    $SideA = ($Backpack.Substring(0,$Middle)).ToCharArray()
+    $SideB = ($Backpack.Substring($Middle)).ToCharArray()
+
+    $Matches = @()
+    ForEach ($AChar in $SideA) {
+        If ($SideB -ccontains $AChar) {
+            $Matches += $AChar
+        }
+    }
+
+    $Unique = $Matches | Select -Unique
+
+    Switch ($Unique) {
+        a {If ($Unique -cmatch "a"){$Priority = 1} Else {$Priority = 27}}
+        b {If ($Unique -cmatch "b"){$Priority = 2} Else {$Priority = 28}}
+        c {If ($Unique -cmatch "c"){$Priority = 3} Else {$Priority = 29}}
+        d {If ($Unique -cmatch "d"){$Priority = 4} Else {$Priority = 30}}
+        e {If ($Unique -cmatch "e"){$Priority = 5} Else {$Priority = 31}}
+        f {If ($Unique -cmatch "f"){$Priority = 6} Else {$Priority = 32}}
+        g {If ($Unique -cmatch "g"){$Priority = 7} Else {$Priority = 33}}
+        h {If ($Unique -cmatch "h"){$Priority = 8} Else {$Priority = 34}}
+        i {If ($Unique -cmatch "i"){$Priority = 9} Else {$Priority = 35}}
+        j {If ($Unique -cmatch "j"){$Priority = 10} Else {$Priority = 36}}
+        k {If ($Unique -cmatch "k"){$Priority = 11} Else {$Priority = 37}}
+        l {If ($Unique -cmatch "l"){$Priority = 12} Else {$Priority = 38}}
+        m {If ($Unique -cmatch "m"){$Priority = 13} Else {$Priority = 39}}
+        n {If ($Unique -cmatch "n"){$Priority = 14} Else {$Priority = 40}}
+        o {If ($Unique -cmatch "o"){$Priority = 15} Else {$Priority = 41}}
+        p {If ($Unique -cmatch "p"){$Priority = 16} Else {$Priority = 42}}
+        q {If ($Unique -cmatch "q"){$Priority = 17} Else {$Priority = 43}}
+        r {If ($Unique -cmatch "r"){$Priority = 18} Else {$Priority = 44}}
+        s {If ($Unique -cmatch "s"){$Priority = 19} Else {$Priority = 45}}
+        t {If ($Unique -cmatch "t"){$Priority = 20} Else {$Priority = 46}}
+        u {If ($Unique -cmatch "u"){$Priority = 21} Else {$Priority = 47}}
+        v {If ($Unique -cmatch "v"){$Priority = 22} Else {$Priority = 48}}
+        w {If ($Unique -cmatch "w"){$Priority = 23} Else {$Priority = 49}}
+        x {If ($Unique -cmatch "x"){$Priority = 24} Else {$Priority = 50}}
+        y {If ($Unique -cmatch "y"){$Priority = 25} Else {$Priority = 51}}
+        z {If ($Unique -cmatch "z"){$Priority = 26} Else {$Priority = 52}}
+    }
+    $Total = $Total + $Priority
+
+}
+
+$Total
