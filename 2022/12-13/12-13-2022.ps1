4bdccbe70dec34c37ae9d2d2775964799b98e3f7
WatchBenGo <ben@thesiebers.net>
2022-12-14 13:30:45 -0600

    Working Code

--- 2022/12-13/12-13-2022.ps1
+++ 2022/12-13/12-13-2022.ps1
@@ -1,6 +1,90 @@
-$Pairs = (Get-Content C:\Users\bsiebers\Desktop\Input.txt -Raw).Trim() -split "`r`n`r`n"
-
-$Indicies = @()
-ForEach ( $Index in (1..$Pairs.Count)) {
-    $Left,$Right = $Pairs[0] -split "`r`n"
+$Queue = [System.Collections.Queue]
+Function ConvertFrom-Text {
+    param (
+        [System.Object]
+        $Argument
+    )
+    Process {
+        [System.Collections.Stack]$Stack = @()
+        Switch($Argument -split ',|(?=[]])|(?<=[[])') {
+            [ {
+                $Stack.Push($Queue::new())
+            }
+            ] {
+                $Store = $Stack.Pop()
+                If($Stack.Count -eq 0){
+                    ,$Store
+                } Else {
+                    $Stack.Peek().Enqueue($Store)
+                }
+            }
+            Default { $Stack.Peek().Enqueue([int]$_) }
+          }
+    }
 }
+
+Function Compare-Items {
+    Param(
+        [System.Object]
+        $a,
+
+        [System.Object]
+        $b
+    )
+    Process {
+        If(($a -is $Queue) -and ($b -is [int])) {
+            Compare-Items $a (,$b -as $Queue)
+        }
+        ElseIf (($a -is [int]) -and ($b -is $Queue)) {
+            Compare-Items (,$a -as $Queue) $b
+        }
+        ElseIf($a -is [int]) {
+            $b - $a
+        }
+        Else {
+            for(;$a -or $b) {
+                if($a.Count -eq 0 -or $b.Count -eq 0) {
+                    return $b.Count - $a.Count
+                }
+                $r = Compare-Items $a.Dequeue() $b.Dequeue()
+                if($r -ne 0) {
+                    return $r
+                }
+            }
+            return 0
+        }
+    }
+  }
+
+$Pairs = Get-Content C:\Users\bsiebers\Desktop\Input.txt
+
+Remove-Variable -ea 0 x,y
+$n=0
+$Catch = switch($Pairs) {
+    {$_-and-not$x} {$x=$_;continue}
+    {$x-and!$y} {
+        $y=$_
+        $a,$b = $x,$y|ForEach-Object{,(ConvertFrom-Text $_)}
+        [pscustomobject]@{
+            n=++$n
+            a=$x
+            b=$y
+            r=Compare-Items $a $b
+        }
+        Remove-Variable x,y
+    }
+}
+
+
+($Catch | Where-Object r -gt 0 | Measure-Object -sum n).Sum
+
+$2 = @($Pairs | Where-Object{$_}; ($keys='[[2]]','[[6]]'))
+class ComparePacketString : System.Collections.IComparer {
+    [int]Compare($x,$y) {
+        return (Compare-Items (ConvertFrom-Text $y) (ConvertFrom-Text $x))
+    }
+}
+[array]::Sort($2, [ComparePacketString]::new())
+$n = 1
+$keys|ForEach-Object{$n*=(1+$2.IndexOf($_))}
+$n
